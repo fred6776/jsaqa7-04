a0a22769655b9975d72b59035d70fcdd12d2a2e7
Farid Mukhametkhanov <fred6776@yandex.com>
2026-01-14 17:59:06 +0300

    task2

--- gh.test.js
+++ gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 }, 60000);
 
 afterEach(() => {
@@ -10,12 +9,18 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  }, 60000);
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
+    expect(title2).toEqual(
+      "GitHub · Change is constant. GitHub keeps you ahead. · GitHub"
+    );
   }, 60000);
 
   test("The first link attribute", async () => {
@@ -29,6 +34,26 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
-    expect(actual).toContain("Sign up for free");
+    expect(actual).toContain("Get started with Team");
   }, 60000);
 });
+
+test("Compare title page copilot", async () => {
+  await page.goto("https://github.com/features/copilot");
+  const title = await page.title();
+  expect(title).toEqual("GitHub Copilot · Your AI pair programmer · GitHub");
+}, 60000);
+
+test("TCompare title actionst", async () => {
+  await page.goto("https://github.com/features/actions");
+  const title = await page.title();
+  expect(title).toEqual("GitHub Actions · GitHub");
+}, 60000);
+
+test("Compare title enterprise", async () => {
+  await page.goto("https://github.com/enterprise");
+  const title = await page.title();
+  expect(title).toEqual(
+    "GitHub Enterprise · The AI-powered developer platform for the agent-ready enterprise · GitHub"
+  );
+}, 60000);
